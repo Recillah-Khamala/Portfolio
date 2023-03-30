55b00c9f14cb55002349813aada41cd202972135
recillah khamala <recillahk@gmail.com>
2023-03-31 00:36:13 +0300

    Add contact component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './components/About';
+import Contact from './components/Contact';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Portfolio from './components/Portfolio';
@@ -14,6 +15,7 @@ const App = () => {
       <About />
       <Portfolio />
       <Stack />
+      <Contact />
       <SocialLinks />
     </div>
   )
--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -64,7 +64,7 @@ const Stack = () => {
   return (
     <div
       name="stack"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-10"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
