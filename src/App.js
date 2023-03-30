f2b81adb9c77897a9198cad42a7ebff94b29fc82
recillah khamala <recillahk@gmail.com>
2023-03-30 16:27:01 +0300

    Add the home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Home from './components/Home'
 import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      < Home />
     </div>
   )
 }
