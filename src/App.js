f0772bbdc7748348fac664fd9cbafbf59c935927
recillah khamala <recillahk@gmail.com>
2023-03-30 23:08:45 +0300

    Add stack component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Portfolio from './components/Portfolio';
 import SocialLinks from './components/SocialLinks';
+import Stack from './components/Stack';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Home />
       <About />
       <Portfolio />
+      <Stack />
       <SocialLinks />
     </div>
   )
